5a70f3d19b00df78550bdd42d6e5013e8322e87b
Tim <posseslash@gmail.com>
2021-11-26 00:40:39 +0100

    design fixes

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -21,17 +21,14 @@ const AboutMe = (props) => {
             <ul className={"about-me__links"}>
                 <li className={"about-me__element"}>
                     <a className={"about-me__link about-me__link--mail"} href="mailto:mail@pkny.ru">
-                        <svg width="20" height="16" viewBox="0 0 20 16" fill="none">
-                            <svg version="1.1" id="Layer_1"  x="0px" y="0px" viewBox="0 0 64 64" >
-                                <path d="M0,0v64h64V0H0z M57,7v7.3L32.3,29.4L7,14.3V7H57z M7,57V22.4l25.4,15.2L57,22.5V57H7z"/>
-                            </svg>
+                        <svg width={"28px"} height={"28px"}  x="0px" y="0px" viewBox="0 0 64 64" >
+                            <path d="M0,0v64h64V0H0z M57,7v7.3L32.3,29.4L7,14.3V7H57z M7,57V22.4l25.4,15.2L57,22.5V57H7z"/>
                         </svg>
-
                     </a>
                 </li>
                <li className={"about-me__element"}>
                     <a className={"about-me__link"} href="https://t.me/ep5il0n">
-                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px" viewBox="0 0 64 64">
+                        <svg width={"28px"} height={"28px"}  x="0px" y="0px" viewBox="0 0 64 64">
                             <path d="M57,7v46.4l-13.5-5.8L42.2,47h-1.4H7V7H57 M64,0H0v54h40.8L64,64V0L64,0z"/>
                             <rect x="21" y="16.5" width="22" height="7"/>
                             <rect x="30" y="30.6" width="13" height="7"/>
@@ -40,7 +37,7 @@ const AboutMe = (props) => {
                 </li>
                 <li className={"about-me__element"}>
                     <a className={"about-me__link"} href="https://instagram.com/pknydesign/">
-                        <svg version="1.1" id="Layer_1" viewBox="0 0 64 64" >
+                        <svg  width={"28px"} height={"28px"}  x="0px" y="0px" viewBox="0 0 64 64" >
                             <g>
                                 <g>
                                     <path d="M32,26.8c2.9,0,5.3,2.4,5.3,5.3c0,2.9-2.4,5.3-5.3,5.3s-5.3-2.4-5.3-5.3C26.7,29.2,29.1,26.8,32,26.8 M32,19.8
